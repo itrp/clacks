e3aea06f96ce6be359cd087e36a29b198a1f10fb
Robin Roestenburg <robin.roestenburg@itrp.com>
2016-01-19 15:46:35 +0100

    Request #1662734: fix writing to log from signal handler

--- lib/clacks/command.rb
+++ lib/clacks/command.rb
@@ -154,12 +154,12 @@ module Clacks
     def setup_signal_handling
       stop_signal = (Signal.list.keys & ['QUIT', 'INT']).first
       Signal.trap(stop_signal) do
-        Clacks.logger.info 'QUIT signal received. Shutting down gracefully.'
+        Thread.new { Clacks.logger.info 'QUIT signal received. Shutting down gracefully.' }
         @service.stop if @service
       end unless stop_signal.nil?
 
       Signal.trap('USR1') do
-        Clacks.logger.info 'USR1 signal received. Rotating logs.'
+        Thread.new { Clacks.logger.info 'USR1 signal received. Rotating logs.' }
         rotate_logs
       end if Signal.list['USR1']
     end
